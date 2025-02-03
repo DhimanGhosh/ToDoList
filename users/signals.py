cebae884de5c06d4347443357f3b4efe873cf53d
DhimanGhosh <dgkiitcsedual@gmail.com>
2025-02-04 04:25:06 +0530

    fixed profile picture addition

--- users/signals.py
+++ users/signals.py
@@ -4,10 +4,10 @@ from django.dispatch import receiver
 from .models import Profile
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:  # Only run when a new user is created
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
+def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
